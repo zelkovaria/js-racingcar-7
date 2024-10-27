4890c64fbd264e4ea532f5d5895a0c0d3700b476
zelkovaria <hyein396@gmail.com>
2024-10-27 20:24:29 +0900

    refactor: ERROR Message 상수화

--- src/Validator.js
+++ src/Validator.js
@@ -1,8 +1,10 @@
+import { ERROR_MESSAGE } from "./constants/Message";
+
 export class CarValidator {
   static #validateUnique(carNames) {
     const noDuplicateCarNames = new Set(carNames);
     if (carNames.length !== noDuplicateCarNames.size) {
-      throw new Error("[ERROR] 자동차 이름을 중복없이 입력해주세요");
+      throw new Error(ERROR_MESSAGE.UNIQUE_CAR_NAMES);
     }
   }
 
@@ -11,10 +13,7 @@ export class CarValidator {
       (car) => car.length < 1 || car.length > 5
     );
 
-    if (isValidLength)
-      throw new Error(
-        "[ERROR] 자동차 이름은 1자 이상 5자 이하로 작성해주세요."
-      );
+    if (isValidLength) throw new Error(ERROR_MESSAGE.INVALID_NAMES_LENGTH);
   }
 
   static validate(inputCars) {
@@ -27,13 +26,13 @@ export class CarValidator {
 export class PlayValidator {
   static #isPositiveNum(playCount) {
     if (playCount <= 0) {
-      throw new Error("[ERROR] 시도 횟수는 양수값으로 입력해주세요.");
+      throw new Error(ERROR_MESSAGE.INVALID_POSITIVE_NUM);
     }
   }
 
   static #isNumber(playCount) {
     if (typeof playCount !== "number") {
-      throw new Error("[ERROR] 시도 횟수는 문자가 아닌 숫자를 입력해주세요.");
+      throw new Error(ERROR_MESSAGE.INVALID_COUNT_TYPE);
     }
   }
 
--- src/constants/Message.js
+++ src/constants/Message.js
@@ -3,3 +3,11 @@ export const MESSAGE = Object.freeze({
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
   READ_TRY_COUNT: "시도할 횟수는 몇 회인가요?",
 });
+
+export const ERROR_MESSAGE = Object.freeze({
+  UNIQUE_CAR_NAMES: "[ERROR] 자동차 이름을 중복없이 입력해주세요",
+  INVALID_NAMES_LENGTH:
+    "[ERROR] 자동차 이름은 1자 이상 5자 이하로 작성해주세요.",
+  INVALID_POSITIVE_NUM: "[ERROR] 시도 횟수는 양수값으로 입력해주세요.",
+  INVALID_COUNT_TYPE: "[ERROR] 시도 횟수는 문자가 아닌 숫자를 입력해주세요.",
+});
