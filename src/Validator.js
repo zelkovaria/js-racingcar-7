b1aa6bd17f6f5105972fec0c5cc877feebf17897
zelkovaria <hyein396@gmail.com>
2024-10-27 17:58:34 +0900

    feat: 자동차 이름 중복, 길이 예외처리 구현

---
+++ src/Validator.js
@@ -0,0 +1,25 @@
+export default class CarValidator {
+  static #validateUnique(carNames) {
+    const noDuplicateCarNames = new Set(carNames);
+    if (carNames.length !== noDuplicateCarNames) {
+      throw new Error("[ERROR] 자동차 이름을 중복없이 입력해주세요");
+    }
+  }
+
+  static #validateLength(carNames) {
+    const isValidLength = carNames.some(
+      (car) => car.length < 1 || car.length > 5
+    );
+
+    if (isValidLength)
+      throw new Error(
+        "[ERROR] 자동차 이름은 1자 이상 5자 이하로 작성해주세요."
+      );
+  }
+
+  static validate(inputCars) {
+    const carNames = inputCars.split(",").map((name) => name.trim());
+    this.#validateUnique(carNames);
+    this.#validateLength(carNames);
+  }
+}
--- src/controller/RacingController.js
+++ src/controller/RacingController.js
@@ -1,10 +1,17 @@
+import { Console } from "@woowacourse/mission-utils";
+import CarValidator from "../Validator.js";
 import InputView from "../views/InputView.js";
 
 class RacingController {
   async #setCarNames() {
-    const carNames = await InputView.readCarNames();
-
-    return carNames;
+    try {
+      const carNames = await InputView.readCarNames();
+      CarValidator.validate(carNames);
+      return carNames.split(",").map((name) => name.trim());
+    } catch (e) {
+      Console.print("[ERROR] 옳지 않은 형식의 입력입니다");
+      throw e;
+    }
   }
 
   async #setPlayCount() {
